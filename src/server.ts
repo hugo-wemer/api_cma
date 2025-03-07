c695cf7d4e75338ffa71cc9c029bb4ceead4949b
Hugo Silva <hugo.silva@treetech.local>
2025-03-07 13:35:05 -0300

    define environment variables

--- src/server.ts
+++ src/server.ts
@@ -9,6 +9,7 @@ import {
 import { fastifySwagger } from '@fastify/swagger'
 import { fastifySwaggerUi } from '@fastify/swagger-ui'
 import { subscribeNewTokenRoute } from './routes/token-route'
+import { env } from './env'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
@@ -32,6 +33,6 @@ app.register(fastifySwaggerUi, {
 
 app.register(subscribeNewTokenRoute)
 
-app.listen({port: 3333, host: '0.0.0.0'}).then(() => {
+app.listen({port: env.PORT, host: '0.0.0.0'}).then(() => {
   console.log('HTTP server running 🚀')
 })
